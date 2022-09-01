63ccbbdecd468a9ddae19332a232deb45ffe270e
mkuczynski11 <mkuczynski11.kontakt@wp.pl>
2022-09-01 10:43:00 +0200

    removed modules and updated main cmake

---
+++ micropython.cmake
@@ -0,0 +1,2 @@
+include(${CMAKE_CURRENT_LIST_DIR}/camera-driver/src/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/st7789/st7789/micropython.cmake)
--- modules/micropython.cmake
+++
@@ -1,2 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/camera/micropython.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/st7789/micropython.cmake)
