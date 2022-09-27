bf5c976a1ec4a0834935bc506252756f6311859a
mkuczynski11 <mkuczynski11.kontakt@wp.pl>
2022-09-27 15:59:15 +0200

    added config file and readme

--- src/esp32_cam/boot.py
+++ src/esp32_cam/boot.py
@@ -1,14 +1,11 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
 try:
     import uos
     import machine
+    from config import microsd_config
     
     sd = machine.SDCard(slot=3, width=1, sck=machine.Pin(14), mosi=machine.Pin(15), miso=machine.Pin(2), cs=machine.Pin(13))
-    uos.mount(sd, '/sd')
+    uos.mount(sd, microsd_config['directory'])
 except Exception as e:
     print("Error ocurred: " + str(e))
     machine.reset()
